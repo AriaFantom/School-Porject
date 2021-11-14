3097af71991404709d68635f3d6e823e5ed063f5
AriaFantom <priyanshudebnath348@gmail.com>
2021-11-14 22:37:13 +0530

    Added the last code

--- p16.py
+++ p16.py
@@ -1,4 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-df=pd.read_csv('Book1.csv')
+df=pd.read_csv('data.csv')
 print(df)
+slices= (df['Population'].head(6))
+states= (df['States'].head(6))
+cols=['m','c','g','b','r','gold']
+exp=[0,0,0,0,0,0.1]
+plt.pie(slices, labels=states,colors=cols,startangle=90, explode=exp,shadow=True,autopct='%.1f%%')
+plt.title('2011 Cencus Data')
+plt.legend()
+plt.show()
