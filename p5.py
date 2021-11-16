5004694bf636ece2a1f0b7a1c0b3328f93d62f76
AriaFantom <priyanshudebnath348@gmail.com>
2021-11-16 21:34:15 +0530

    Capital Letters

--- p5.py
+++ p5.py
@@ -1,8 +1,7 @@
-
 import pandas as pd 
 data={'Name':['Aman','Rohit','Deepika','Aman','Deepika','Sohit','Geeta'], 
 'Sales':[8500,4500,4600,8500,4600,9600,8400]} 
 sales=pd.DataFrame(data) 
 
 duplicated = sales[sales.duplicated(keep=False)]
-print("duplicate Row:\n",duplicated)
+print("Duplicate Row:\n",duplicated)
